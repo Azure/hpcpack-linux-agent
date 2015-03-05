896542c3e334d327824dd3cad0eb03f79b065049
Evan Cui <evanc@microsoft.com>
2015-03-04 23:30:01 -0800

    Fix the task long running by adding task requeue count

--- nodemanager/arguments/ProcessStartInfo.cpp
+++ nodemanager/arguments/ProcessStartInfo.cpp
@@ -6,10 +6,21 @@ using namespace hpc::utils;
 
 ProcessStartInfo::ProcessStartInfo(
     std::string&& cmdLine,
+    std::string&& stdIn,
+    std::string&& stdOut,
+    std::string&& stdErr,
     std::string&& workDir,
+    int taskRequeueCount,
     std::vector<long>&& affinity,
     std::map<std::string, std::string>&& enviVars) :
-    CommandLine(std::move(cmdLine)), WorkDirectory(std::move(workDir)), Affinity(std::move(affinity)), EnvironmentVariables(std::move(enviVars))
+    CommandLine(std::move(cmdLine)),
+    StdInText(std::move(stdIn)),
+    StdOutText(std::move(stdOut)),
+    StdErrText(std::move(stdErr)),
+    WorkDirectory(std::move(workDir)),
+    TaskRequeueCount(taskRequeueCount),
+    Affinity(std::move(affinity)),
+    EnvironmentVariables(std::move(enviVars))
 {
 }
 
@@ -18,7 +29,11 @@ ProcessStartInfo ProcessStartInfo::FromJson(const web::json::value& jsonValue)
 {
     ProcessStartInfo startInfo(
         JsonHelper<std::string>::Read("commandLine", jsonValue),
+        JsonHelper<std::string>::Read("stdin", jsonValue),
+        JsonHelper<std::string>::Read("stdout", jsonValue),
+        JsonHelper<std::string>::Read("stderr", jsonValue),
         JsonHelper<std::string>::Read("workingDirectory", jsonValue),
+        JsonHelper<int>::Read("taskRequeueCount", jsonValue),
         JsonHelper<std::vector<long>>::Read("affinity", jsonValue),
         JsonHelper<std::map<std::string, std::string>>::Read("environmentVariables", jsonValue));
 
