418ae90cabe1b70f37c350345026bee30644608b
FAREAST\chezhang <chezhang@microsoft.com>
2019-05-09 23:34:37 +0800

    replace mpi command in the whole command line instead of just the beginning

--- nodemanager/filters/AdjustMpiCommand.py
+++ nodemanager/filters/AdjustMpiCommand.py
@@ -7,19 +7,17 @@ j = json.load(sys.stdin)
 commandLine = j['m_Item2'].get('commandLine')
 mpiSource = j['m_Item2']['environmentVariables'].get('CCP_MPI_SOURCE')
 if commandLine and mpiSource:
-    if commandLine.startswith('mpirun ') or commandLine.startswith('mpiexec '):
-        machineFileOption = '-machinefile $CCP_MPI_HOSTFILE'
-        commandLineSplit = commandLine.split(' ')
-        if mpiSource.endswith('/mpirun') or mpiSource.endswith('/mpiexec'):
-            commandLineSplit[0] = mpiSource
-        elif mpiSource.endswith('/'):
-            commandLineSplit[0] = '{}{}'.format(mpiSource, commandLineSplit[0])
-        elif mpiSource.endswith('/mpivars.sh'):
-            commandLineSplit[0] = 'source {}; {}'.format(mpiSource, commandLineSplit[0])
-        else:
-            commandLineSplit[0] = '{}/{}'.format(mpiSource, commandLineSplit[0])
-        commandLineSplit.insert(1, machineFileOption)
-        commandLine = ' '.join(commandLineSplit)
-        j['m_Item2']['commandLine'] = commandLine
+    if mpiSource.endswith('/mpirun') or mpiSource.endswith('/mpiexec'):
+        mpiCommand = '/'.join(mpiSource.split('/')[:-1]) + '/mpiexec'
+    elif mpiSource.endswith('/'):
+        mpiCommand = '{}mpiexec'.format(mpiSource)
+    elif mpiSource.endswith('/mpivars.sh'):
+        mpiCommand = 'source {}; mpiexec'.format(mpiSource)
+    else:
+        mpiCommand = '{}/mpiexec'.format(mpiSource)
+    mpiCommand += ' '
+    if 'CCP_MPI_HOSTFILE_FORMAT' in j['m_Item2']['environmentVariables']:
+        mpiCommand += '-machinefile $CCP_MPI_HOSTFILE '
+    j['m_Item2']['commandLine'] = commandLine.replace('mpiexec ', mpiCommand).replace('mpirun ', mpiCommand)
 
 print(json.dumps(j))
