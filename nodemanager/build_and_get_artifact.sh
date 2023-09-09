a703aa8d7cf3b15b47e2a06a6e5386e70f1f7f56
Zheyu Shen <zheyushen@microsoft.com>
2023-09-09 18:04:28 +0800

    add nuget

--- nodemanager/build_and_get_artifact.sh
+++ nodemanager/build_and_get_artifact.sh
@@ -6,8 +6,12 @@ source /hbb_exe/activate
 
 set -x
 
+rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
 yum update -y
-yum install perl-IPC-Cmd kernel-devel -y
+yum install perl-IPC-Cmd kernel-devel mono-complete -y
+curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+alias nuget="mono /usr/local/bin/nuget.exe"
 
 cd /hpcpack-linux-agent/nodemanager
 ../vcpkg/bootstrap-vcpkg.sh
