8159b686bf8e5efe3e8fbe2d2e5a9ec14c5a2f30
Robert Zhang <louirobert@gmail.com>
2024-11-05 22:46:40 +0800

    Add unit test for LoopWork

---
+++ src/NodeAgent.Test/Mocks/MockOptionsMonitor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace NodeAgent.Test.Mocks;
+
+public class MockOptionsMonitor<T> : IOptionsMonitor<T> where T : class
+{
+    private T _value;
+
+    public MockOptionsMonitor(T value)
+    {
+        _value = value;
+    }
+
+    public T CurrentValue => _value;
+
+    public T Get(string? name)
+    {
+        throw new NotSupportedException();
+    }
+
+    public IDisposable? OnChange(Action<T, string?> listener)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ src/NodeAgent.Test/Utils/LoopWorkTest.cs
@@ -0,0 +1,82 @@
+﻿
+using NodeAgent.Test.Mocks;
+using NodeAgent.Utils;
+using static NodeAgent.Utils.LoopWork;
+
+namespace NodeAgent.Test.Utils;
+
+public class LoopWorkTest
+{
+    [Theory]
+    [InlineData(1, 1, 1500, 1)]
+    [InlineData(1, 1, 2500, 2)]
+    [InlineData(0, 1, 1500, 2)]
+    [InlineData(0, 1, 2500, 3)]
+    public async Task TestStart(int HoldSeconds, int IntervalSeconds, int waitMS, int expectedWorkCount)
+    {
+        var options = new StartOptions()
+        {
+            HoldSeconds = HoldSeconds,
+            IntervalSeconds = IntervalSeconds,
+            ErrorRetryMultiplyFactor = 1,
+        };
+        var optMonitor = new MockOptionsMonitor<StartOptions>(options);
+        var cts = new CancellationTokenSource();
+        var workCount = 0;
+        var work = (CancellationToken token) =>
+        {
+            workCount++;
+            return Task.FromResult(true);
+        };
+        var errorCount = 0;
+        var onError = (int retry, CancellationToken token) =>
+        {
+            errorCount++;
+            return Task.CompletedTask;
+        };
+
+        _ = LoopWork.StartAsync(work, onError, cts.Token, optMonitor);
+        await Task.Delay(waitMS);
+        cts.Cancel();
+
+        Assert.Equal(expectedWorkCount, workCount);
+        Assert.Equal(0, errorCount);
+    }
+
+    [Theory]
+    [InlineData(0, 1, 1, 3, 3500, 4, 3)]
+    [InlineData(1, 1, 1, 3, 3500, 3, 3)]
+    public async Task TestStartError(int HoldSeconds, int IntervalSeconds, int retryMultiplyFactor, 
+        int countBeforeOk, int waitMS, int expectedWorkCount, int expectedErrorCount)
+    {
+        var options = new StartOptions()
+        {
+            HoldSeconds = HoldSeconds,
+            IntervalSeconds = IntervalSeconds,
+            ErrorRetryMultiplyFactor = retryMultiplyFactor,
+        };
+        var optMonitor = new MockOptionsMonitor<StartOptions>(options);
+        var cts = new CancellationTokenSource();
+        var workCount = 0;
+        var work = (CancellationToken token) =>
+        {
+            workCount++;
+            return Task.FromResult(workCount > countBeforeOk);
+        };
+        var errorCount = 0;
+        var onError = (int retry, CancellationToken token) =>
+        {
+            Assert.True(workCount <= countBeforeOk);
+            Assert.Equal(workCount - 1, retry);
+            errorCount++;
+            return Task.CompletedTask;
+        };
+
+        _ = LoopWork.StartAsync(work, onError, cts.Token, optMonitor);
+        await Task.Delay(waitMS);
+        cts.Cancel();
+
+        Assert.Equal(expectedWorkCount, workCount);
+        Assert.Equal(expectedErrorCount, errorCount);
+    }
+}
