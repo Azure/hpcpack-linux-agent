c8d0ebfca78691e9efc731f1335337bcadd1e9c6
Robert Zhang <louirobert@gmail.com>
2025-02-13 21:15:27 +0800

    Add JobTaskExecutorTest.TestStartJobAndTaskAsync

---
+++ src/NodeAgent.Test/Mocks/MockSchedulerApiClientForJobTaskExecutor.cs
@@ -0,0 +1,37 @@
+﻿using NodeAgent.Models;
+using NodeAgent.Services;
+
+namespace NodeAgent.Test.Mocks;
+
+public class MockSchedulerApiClientForJobTaskExecutor : ISchedulerApiClient
+{
+    public class TaskCompletionCall
+    {
+        public string? Uri { get; set; }
+
+        public TaskCompletionEventArgs? Args { get; set; }
+    }
+
+    public IList<TaskCompletionCall> TaskCompletionCalls { get; private set; } = new List<TaskCompletionCall>();
+
+    public Task<Tuple<IEnumerable<HostEntry>?, string?>?> GetHostsAsync(string? updateId, CancellationToken cancelToken = default)
+    {
+        throw new NotSupportedException();
+    }
+
+    public Task<int> RegisterAsync(RegisterInfo info, CancellationToken cancelToken = default)
+    {
+        throw new NotSupportedException();
+    }
+
+    public Task<int> ReportHeartbeatAsync(NodeInfo nodeInfo, CancellationToken cancelToken = default)
+    {
+        throw new NotSupportedException();
+    }
+
+    public Task ReportTaskCompletionAsync(string uri, TaskCompletionEventArgs args, CancellationToken cancelToken = default)
+    {
+        TaskCompletionCalls.Add(new TaskCompletionCall() { Uri = uri, Args = args });
+        return Task.CompletedTask;
+    }
+}
---
+++ src/NodeAgent.Test/Servcies/JobTaskExecutorTest.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using NodeAgent.Models;
+using NodeAgent.Services;
+using NodeAgent.Test.Mocks;
+using System.Runtime.Versioning;
+using Xunit.Abstractions;
+
+namespace NodeAgent.Test.Servcies;
+
+[SupportedOSPlatform("linux")]
+public class JobTaskExecutorTest : TestBase
+{
+    private MockSchedulerApiClientForJobTaskExecutor _schedulerApiClient;
+
+    private ISystemService _systemService;
+
+    private JobTaskExecutor _jobTaskExecutor;
+
+    public JobTaskExecutorTest(ITestOutputHelper output) : base(output)
+    {
+        _schedulerApiClient = new MockSchedulerApiClientForJobTaskExecutor();
+
+        var sysLogger = LoggerFactory.CreateLogger<SystemService>();
+        _systemService = new SystemService(sysLogger);
+
+        var taskFactoryLogger = LoggerFactory.CreateLogger<TaskProcessFactory>();
+        var taskFactory = new TaskProcessFactory(taskFactoryLogger, LoggerFactory, _systemService);
+
+        var logger = LoggerFactory.CreateLogger<JobTaskExecutor>();
+        var flag = new ResyncFlag();
+
+        _jobTaskExecutor = new JobTaskExecutor(logger, _schedulerApiClient, flag, _systemService, taskFactory);
+    }
+
+    [Fact]
+    public async Task TestStartJobAndTaskAsync()
+    {
+        var args = new StartJobAndTaskArgs()
+        {
+            JobId = 1,
+            TaskId = 2,
+            StartInfo = new ProcessStartInfo() { CommandLine = "sleep 1 && hostname" },
+            UserName = TestUser.RandomName,
+            Password = "password",
+        };
+        var callbackUri = "http://callback";
+        await _jobTaskExecutor.StartJobAndTaskAsync(args, callbackUri);
+        var jobCount = _jobTaskExecutor.GetJobCount();
+        Assert.Equal(1, jobCount);
+        var taskCount = _jobTaskExecutor.GetTaskCount();
+        Assert.Equal(1, taskCount);
+        var taskInfo = _jobTaskExecutor.GetJobs().Single().Tasks.Single().Value;
+        Assert.Equal(1, taskInfo.JobId);
+        Assert.Equal(2, taskInfo.TaskId);
+        Assert.Equal(0, taskInfo.TaskRequeueCount);
+        Assert.False(taskInfo.Exited);
+
+        await Task.Delay(3000);
+        TestOut.OutputObject(taskInfo);
+
+        Assert.True(taskInfo.Exited);
+        Assert.Equal(0, taskInfo.ExitCode);
+        Assert.Contains(_systemService.HostName, taskInfo.Message);
+
+        Assert.Single(_schedulerApiClient.TaskCompletionCalls);
+        var call = _schedulerApiClient.TaskCompletionCalls.Single();
+        Assert.Equal(callbackUri, call.Uri);
+        Assert.Equal(1, call.Args?.JobId);
+        Assert.Equal(2, call.Args?.TaskInfo.TaskId);
+        Assert.Equal(taskInfo, call.Args?.TaskInfo);
+
+        //NOTE: Job doesn't get removed automatically after all tasks are done.
+        jobCount = _jobTaskExecutor.GetJobCount();
+        Assert.Equal(1, jobCount);
+
+        taskCount = _jobTaskExecutor.GetTaskCount();
+        Assert.Equal(0, taskCount);
+    }
+}
